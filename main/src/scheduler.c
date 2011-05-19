9df2b50f1e453eb1483dfd810fa919122f25366f
Brian Lalor <blalor@bravo5.org>
2011-05-19 10:23:17 -0400

    Comments and code cleanup
    (premature?) optimization of scheduler_invoke_tasks() to minimize chance of
    counter and target getting modified mid-read by ISR.

--- main/src/scheduler.c
+++ main/src/scheduler.c
@@ -28,14 +28,22 @@ void scheduler_tick() {
 
 void scheduler_invoke_tasks() {
     if (tick_occurred) {
+        uint16_t counter, target;
+        
         for (int i = 0; i < task_count; i++) {
+            counter = tasks[i]->counter;
+            target = tasks[i]->target;
+            
             if (
                 tasks[i]->enabled &&
-                (tasks[i]->counter > 0) &&
-                (tasks[i]->counter >= tasks[i]->target)
+                (counter > 0) &&
+                (counter >= target)
             ) {
+                // counter could be reset before or after task invocation. If 
+                // done before, task is started at (approximately) regular 
+                // intervals.  If done after, task is started after its scheduled
+                // period after execution.
                 tasks[i]->counter = 0;
-
                 tasks[i]->task_callback();
             }
         }
